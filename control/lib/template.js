ab8ebc8dc35ac401cac504deefbb257519e8d7b2
Pablo Alonso Garcia <alonsogarciapablo@gmail.com>
2015-03-12 18:52:04 +0100

    Removes mustache.js from /lib and comments out the only use of it.

--- control/lib/template.js
+++ control/lib/template.js
@@ -6,14 +6,18 @@ wax.template = function(x) {
     // Clone the data object such that the '__[format]__' key is only
     // set for this instance of templating.
     template.format = function(options, data) {
-        var clone = {};
-        for (var key in data) {
-            clone[key] = data[key];
-        }
-        if (options.format) {
-            clone['__' + options.format + '__'] = true;
-        }
-        return wax.u.sanitize(Mustache.to_html(x, clone));
+
+        // mustache.js has been removed as a dependency
+        throw new Error('mustache.js templates are no longer supported');
+
+        // var clone = {};
+        // for (var key in data) {
+        //     clone[key] = data[key];
+        // }
+        // if (options.format) {
+        //     clone['__' + options.format + '__'] = true;
+        // }
+        // return wax.u.sanitize(Mustache.to_html(x, clone));
     };
 
     return template;
